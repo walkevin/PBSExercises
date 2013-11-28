6731ecc73ef010d326b32a94e726697176fa2a91
walkevin <wkevin@student.ethz.ch>
2013-11-28 23:36:04 +0100

    Aufgeräumt und brandneues beispiel: ExampleAnimInstanced.
    Gleich wie ExampleAnim, aber statt Punkte kommen kleine Bälle.

--- code/project/GLUTFramework/examples/main.cpp
+++ code/project/GLUTFramework/examples/main.cpp
@@ -22,7 +22,7 @@
  * THE SOFTWARE.
  */
 
-#include "RotatingCamera.h"
+#include "ExampleAnimInstanced.h"
 //#include "ViewerShader.h"
 //#include "ViewerPoints.h"
 #include "../src/GlutFramework.h"
@@ -39,7 +39,7 @@ using namespace glutFramework;
 int main(int argc, char *argv[]) {
 	
 //    GlutFramework* framework = new ViewerShader();
-	GlutFramework* framework = new RotatingCamera();
+	GlutFramework* framework = new ExampleAnimInstanced();
 //	GlutFramework* framework = new GlutFramework();
 	framework->setLookAt(0.0, 2.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	framework->startFramework(argc, argv);
